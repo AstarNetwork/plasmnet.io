9d2a203a7c9190cb78b74412f495eab8405bb0e0
”roy1210” <rswkure@gmail.com>
2020-02-17 12:33:20 +0800

    Try countdown for mobile

--- plasmnet/src/components/Title.tsx
+++ plasmnet/src/components/Title.tsx
@@ -7,7 +7,7 @@ import "../styles/animation.scss";
 import { customMedia } from "../styles/globalStyle";
 import { theme } from "../styles/theme";
 import LockdropInfo from "./LockdropInfo";
-interface Props {}
+interface Props { }
 
 const Title: React.FC<Props> = () => {
   return (
@@ -90,7 +90,7 @@ const Title: React.FC<Props> = () => {
         <div className="right">
           <div className="ui SlideUp one">
             <div className="lockdrop">
-              <LockdropInfo countdownDate="2021-1-1 00:00:00" />
+              <LockdropInfo countdownDate="2021-01-01 00:00:00" />
             </div>
             <div className="app-buttons SlideUp two">
               <div>
