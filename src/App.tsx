e34ddbfe2eb85698f286f5c44e6fc893e18c9973
Hoon Kim <hoonsubin@icloud.com>
2020-02-22 15:51:45 +0900

    used shorthand fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import ProgressBar from "react-scroll-progress-bar";
-import Header from './components/Header'
-import HeaderSmall from './components/HeaderSmall'
-import Footer from './components/Footer'
-import { GlobalStyle } from "./styles/globalStyle";
+import ProgressBar from 'react-scroll-progress-bar';
+import Header from './components/Header';
+import HeaderSmall from './components/HeaderSmall';
+import Footer from './components/Footer';
+import { GlobalStyle } from './styles/globalStyle';
 import FooterPush from './components/FooterPush';
 import MainContents from './components/MainContents';
-import "./styles/animation.scss"
-import { theme } from "./styles/theme";
+import './styles/animation.scss';
+import { theme } from './styles/theme';
 
 const App = () => {
   return (
-    <React.Fragment>
-      <ProgressBar height="4px" bgcolor={theme.colors.yellow} />
+    <>
+      <ProgressBar height='4px' bgcolor={theme.colors.yellow} />
       <Header />
       <HeaderSmall />
       <MainContents />
       <FooterPush />
       <Footer />
       <GlobalStyle />
-    </React.Fragment>
+    </>
   );
 }
 
