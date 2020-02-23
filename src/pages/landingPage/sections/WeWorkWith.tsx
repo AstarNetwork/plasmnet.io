b67dea94faad060fb55e956dd951cf1c66d5da90
Hoon Kim <hoonsubin@icloud.com>
2020-02-23 16:41:56 +0900

    added sponsor section

--- src/pages/landingPage/sections/WeWorkWith.tsx
+++ src/pages/landingPage/sections/WeWorkWith.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+//import Card from '@material-ui/core/Card';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-import { TeamMembers } from '../../../database/teamMembers';
+import { Sponsors } from '../../../database/links';
 
 const useStyles = makeStyles(theme => ({
-    icon: {
-        marginLeft: theme.spacing(1),
-        marginTop: theme.spacing(1),
-        color: '#7FD1FF',
+    logo: {
+        maxHeight: '100%',
+        maxWidth: '100%',
+        height: 'auto',
+        width: 'auto'
     },
     heroContent: {
         //backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(8, 0, 6),
+        //background: '#47BCFF',
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
     },
     cardGrid: {
         paddingTop: theme.spacing(8),
@@ -31,13 +35,25 @@ const useStyles = makeStyles(theme => ({
     cardContent: {
         flexGrow: 1,
     },
+    btnPrimary: {
+        background: 'linear-gradient(45deg, #1d417f 30%, #2e8ec0 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
+    media: {
+        height: 210,
+    }
 }));
 
-const cards = TeamMembers;
+const cards = Sponsors;
 
 interface Props { }
 
-const WeWorkWith: React.FC<Props> = () => {
+const OurTeam: React.FC<Props> = () => {
     const classes = useStyles();
 
     return (
@@ -55,18 +71,16 @@ const WeWorkWith: React.FC<Props> = () => {
                     {/* End hero unit */}
                     <Grid container spacing={4}>
                         {cards.map(card => (
-                            <Grid item key={card.id} xs={12} sm={6} md={4}>
-                                <Card className={classes.card}>
-                                    {/* <card.image style={{ fontSize: 60 }} className={classes.icon}></card.image> */}
-                                    <CardContent className={classes.cardContent}>
-                                        <Typography gutterBottom variant='h5' component='h2'>
-                                            {card.heading}
-                                        </Typography>
-                                        <Typography>
-                                            {card.content}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
+                            <Grid item key={card.className} xs={12} sm={6} md={4}>
+                                <div className="sponsor" key={card.className}>
+                                    <a href={card.link} rel="noopener noreferrer" target="_blank">
+                                        <img
+                                            src={card.image}
+                                            alt={card.description}
+                                            className={classes.logo}
+                                        />
+                                    </a>
+                                </div>
                             </Grid>
                         ))}
                     </Grid>
@@ -76,4 +90,4 @@ const WeWorkWith: React.FC<Props> = () => {
     );
 }
 
-export default WeWorkWith;
+export default OurTeam;
