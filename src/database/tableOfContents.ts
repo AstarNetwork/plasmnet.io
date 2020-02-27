c9f31ffb1148d8d0d6dff68666e5cc98ab70465a
satellitex <s.a.t.e.3.ths@gmail.com>
2020-02-27 14:18:25 +0900

    fix tableOfContents

--- src/database/tableOfContents.ts
+++ src/database/tableOfContents.ts
@@ -2,42 +2,37 @@ import { TableOfItem } from '../types/types';
 
 export const TableOfContents: TableOfItem[] = [
     {
-        id: 1,
-        content: 'Launch UI',
-        link: 'ui-id',
-    },
-    {
         id: 2,
-        content: 'Lockdrop Information',
-        link: 'ui-id',
-    },
-    {
-        id: 3,
         content: 'About Plasm Networks',
         link: 'issues-section',
     },
     {
         id: 3,
         content: 'Plasm Networks archives',
-        link: 'aboutToken-section',
+        link: 'ourWork-section',
     },
     {
         id: 4,
+        content: 'About PLM Tokens',
+        link: 'aboutToken-section',
+    },
+    {
+        id: 5,
         content: 'Help us',
         link: 'help-section',
     },
     {
-        id: 5,
+        id: 6,
         content: 'Sponsors',
         link: 'sponsor-section',
     },
     {
-        id: 6,
+        id: 7,
         content: 'Roadmap',
         link: 'roadmap-section',
     },
     {
-        id: 7,
+        id: 8,
         content: 'Community',
         link: 'community-section',
     },
