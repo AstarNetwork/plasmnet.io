12d84348e1f3f7b8b3f8af9aed3d827d2ccece20
Shoe Kure <rswkure@gmail.com>
2020-06-04 20:04:06 +0800

    animation: hovering animation at Title with framer motion (#34)

---
+++ src/styles/titleTransitAnimation.ts
@@ -0,0 +1,11 @@
+export const TitleVariants = {
+    whileHover: {
+        scale: 1.25,
+        cursor: 'pointer',
+    },
+};
+
+export const TitleTransition = {
+    type: 'spring',
+    stiffness: 190,
+};
