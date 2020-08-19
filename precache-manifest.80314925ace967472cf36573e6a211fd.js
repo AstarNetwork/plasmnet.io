00f7600cc6027c24986db41cca7da0be0e310604
hoonsubin <hoonsubin@users.noreply.github.com>
2020-08-19 09:02:12 +0000

    deploy: 84233e159c467105ab54e902dca891e3cbeeb6c3

---
+++ precache-manifest.80314925ace967472cf36573e6a211fd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc27545d7af591ba19f1819ffd8ca75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "249e78373a614fb543b9",
+    "url": "/static/css/2.c427c82d.chunk.css"
+  },
+  {
+    "revision": "c4cc3862e2901582f45f",
+    "url": "/static/css/main.070b8639.chunk.css"
+  },
+  {
+    "revision": "249e78373a614fb543b9",
+    "url": "/static/js/2.69f719a2.chunk.js"
+  },
+  {
+    "revision": "d7f87556689d2eac04fb17102c496c55",
+    "url": "/static/js/2.69f719a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4cc3862e2901582f45f",
+    "url": "/static/js/main.3300f53c.chunk.js"
+  },
+  {
+    "revision": "1ca56b6e53ce1dc83ae6",
+    "url": "/static/js/runtime-main.0a3c20bd.js"
+  },
+  {
+    "revision": "6da6b86916b365d44c64c2683f775879",
+    "url": "/static/media/acala.6da6b869.svg"
+  },
+  {
+    "revision": "c1e83c1a3121c64105822a7e83eac1f9",
+    "url": "/static/media/co-nect.c1e83c1a.png"
+  },
+  {
+    "revision": "0d95a1125805441b2c54bdbd6a1077be",
+    "url": "/static/media/crypto-economics-lab.0d95a112.png"
+  },
+  {
+    "revision": "936512f6e3176c036fac414aa2af111f",
+    "url": "/static/media/discord.936512f6.svg"
+  },
+  {
+    "revision": "0a3a0978834655eadc804efda1d3a443",
+    "url": "/static/media/github.0a3a0978.svg"
+  },
+  {
+    "revision": "4dde1f4872eb8b5e5b1f081ca4df6242",
+    "url": "/static/media/longhash.4dde1f48.png"
+  },
+  {
+    "revision": "e814f1e5c830f4e5aa29e3c4d78b87fc",
+    "url": "/static/media/plasm-logo.e814f1e5.png"
+  },
+  {
+    "revision": "4c49693b4625bab003b51956f3ce29bf",
+    "url": "/static/media/telegram.4c49693b.svg"
+  },
+  {
+    "revision": "7137286db3c9254ace982646907e65a3",
+    "url": "/static/media/twitter.7137286d.svg"
+  },
+  {
+    "revision": "6ca6b71d43e8e661ceb034b26ca3fc35",
+    "url": "/static/media/web3-foundation.6ca6b71d.png"
+  }
+]);
